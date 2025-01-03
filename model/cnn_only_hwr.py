13db5edd345d8556bd0cfbfee9ca9831801e97b0
tuanha <tuanha@digi-texx.vn>
2025-01-03 17:54:16 +0700

    Check cnn input

--- model/cnn_only_hwr.py
+++ model/cnn_only_hwr.py
@@ -97,7 +97,7 @@ class CNNOnlyHWR(nn.Module):
 
 
     def forward(self, input_cn, style=None):
-        # ic(input_cn.shape)
+        ic(input_cn.shape)
         if self.pad is not None:
             input_cn=self.pad(input_cn)
         # ic(input_cn.shape)
